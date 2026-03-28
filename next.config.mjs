19f9920e27e40d6efefc9ccfd1130c1dd3933bba
agent462 <agent462@gmail.com>
2026-03-28 18:20:05 -0500

    release v0.21.0

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'));
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   env: {
     NEXT_PUBLIC_APP_VERSION: pkg.version,
   },
